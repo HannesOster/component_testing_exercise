f588094550430b98d099e55bcd4280b62feccba8
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-04 12:11:54 +0200

    remove test

--- components/Player/Player.test.js
+++ components/Player/Player.test.js
@@ -2,12 +2,6 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {
-  render(<Player />);
-
-  const buttons = screen.getAllByRole("button");
-
-  expect(buttons).toHaveLength(2);
-});
+test("renders player information and two buttons", () => {});
 
 test("calls callbacks when increasing or decreasing score", async () => {});
