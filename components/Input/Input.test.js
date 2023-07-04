037e463e04343920bd141f2b70058204998ec900
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-04 12:09:03 +0200

    add input test

--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -2,6 +2,25 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input name="input" labelText="text" />);
 
-test("calls callback on every user input", async () => {});
+  const input = screen.getByLabelText("text");
+
+  expect(input).toHaveAttribute("name", "input");
+
+  // `toHaveAttribute(attributeName, value)`.
+});
+
+test("calls callback on every user input", async () => {
+  const onChange = jest.fn();
+
+  const user = userEvent.setup();
+
+  render(<Input name="input" labelText="text" onChange={onChange} />);
+
+  const input = screen.getByLabelText("text");
+  await user.type(input, "5");
+
+  expect(onChange).toHaveBeenCalledTimes(1);
+});
--- components/Player/Player.test.js
+++ components/Player/Player.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player />);
+
+  const buttons = screen.getAllByRole("button");
+
+  expect(buttons).toHaveLength(2);
+});
 
 test("calls callbacks when increasing or decreasing score", async () => {});
